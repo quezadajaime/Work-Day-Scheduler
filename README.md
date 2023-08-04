03f17a2f7909f45146dc2523f9b915f4b80bdfd4
Jaime <quezadajaime@ymail.com>
2023-08-03 17:58:17 -0700

    cleaned up HTML/script and added README

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # Work Day Scheduler Starter Code
+
+## Description
+
+This webpage allows the user to create an agenda for a 9am - 5pm work schedule.
+
+The time and day will is visible on the Header. The time will be updated upon page refreshment.
+
+![Work Day Scheduler](https://github.com/quezadajaime/Work-Day-Scheduler/assets/136545874/8994b858-5650-4ea4-99a0-a93600f4bc8b)
+
+## Instructions
+
+Click on the desired block hour and input the event. Click the saved button for the block hour.
+
+## Installation
+
+Follow the following links for the weekday scheduler
+
+Github
+
+https://github.com/quezadajaime/Work-Day-Scheduler.git
+--------------------------------------------------------
+Webpage
+
+https://quezadajaime.github.io/Work-Day-Scheduler/
+
+## Usage
+
+This page is ment to keep track of the daily task during a working day.
+
+##
+MDN Web Docs
+
+edX Bootcamp
+
+# License
+
+MIT License
--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+
 var todayDate = dayjs().format('dddd, MMM D YYYY hh:mm a');
 $("#currentDay").html(todayDate);
 
@@ -15,6 +13,7 @@ $(document).ready(function () {
       localStorage.setItem(time, text);
   })
 
+//retrive the items from local storage
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
 $("#hour-10 .description").val(localStorage.getItem("hour-10"));
 $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -32,7 +31,7 @@ function timeTracker() {
   $(".time-block").each(function () {
       var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
 
-      // To check the time and add the classes for background indicators
+      // Checking the time to the time of each block
       if (blockTime < timeNow) {
           $(this).removeClass("future");
           $(this).removeClass("present");
@@ -52,33 +51,4 @@ function timeTracker() {
 }
 timeTracker();
 
-
-
-
 })
-
-
-
-
-
-  
-
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
